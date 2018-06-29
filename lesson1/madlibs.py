4edb2c73277b99198223bf845fe90d08df6c6186
Jennifer Hammelman <jhammelman@gmail.com>
2018-06-29 16:34:55 -0400

    fix comments

--- lesson1/madlibs.py
+++ lesson1/madlibs.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 print "------Correctly working program-------"
-# There is a bug in this program with the
-# variable names!
 noun1 = raw_input("Give me a noun: ")
 
 adjective = raw_input("Give me an adjective: ")
@@ -39,4 +37,4 @@ noun = raw_input("Give me a noun: ")
 
 sentence = "After hiding the painting in his " + raw_input + " for two years, he grew " + adjective + " and tried to sell it to a/an " + noun + " in Florence, but was caught."
 
-print sentencex
+print sentence
