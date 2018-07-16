f3df53b4411a6e8e58997f55c1eaf4e8a8824ada
Jennifer Hammelman <jhammelman@gmail.com>
2018-07-16 10:22:42 -0400

    lesson 3 template

---
+++ lesson3/my_text_adventure_game.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+print("Here is where you can start your own text adventure game!")
+ans = raw_input("Enter a question for the player, make sure to specify their options for response (y or n) ")
+# ans holds the string of the user's response
+# start with an if statement checking the possible
+# strings for ans. Remember that anything you want to happen
+# based on the result of the if statement has to be indented
+# example:
+# if ans == "y":
+#     print("you said yes")
+# else:
+#     print("you said no")
+
+print("The End")
